a41914bdabd58ff774104a19c487eb1f067cf1ec
Abhishek Mishra <153032227+mrradhey18@users.noreply.github.com>
2026-05-21 22:07:01 +0530

    Add cache-busting query parameter to loadJSON

--- public/js/utils.js
+++ public/js/utils.js
@@ -213,7 +213,7 @@ const Utils = (() => {
    */
   async function loadJSON(path) {
     try {
-      const res = await fetch(path);
+      const res = await fetch(url + '?v=' + Date.now());
       if (!res.ok) throw new Error(`HTTP ${res.status} for ${path}`);
       return await res.json();
     } catch (err) {
